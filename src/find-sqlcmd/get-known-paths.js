193c8cdea95e274477558273dd36a6d86a134555
Michael J. Ryan <mryan@runbeck.net>
2020-01-23 10:08:14 -0700

    fix getKnownPaths, align with which package detection for win32, cygwin, msys environments

--- src/find-sqlcmd/get-known-paths.js
+++ src/find-sqlcmd/get-known-paths.js
@@ -1,17 +1,31 @@
-export const knownWindowsPaths = [
+const winPaths = [
   'C:/Program Files/Microsoft SQL Server/Client SDK/ODBC/170/Tools/Binn',
+  'C:/Program Files/Microsoft SQL Server/Client SDK/ODBC/130/Tools/Binn',
   'C:/Program Files/Microsoft SQL Server/110/Tools/Binn',
 ];
 
+export const nowMap = p => p.replace(/^(\w)\:/, m => `/${m[0].toLowerCase()}`);
+export const knownNixOnWindowsPaths = winPaths.map(nowMap);
+
+export const winMap = p => p.replace(/\//g, '\\');
+export const knownWindowsPaths = winPaths.map(winMap);
+
 export const knownLinuxPaths = ['/opt/mssql-tools/bin'];
 
-export default (platform = process.platform) => {
-  switch (platform) {
-    case 'win32':
-      return knownWindowsPaths;
-    case 'linux':
-      return knownLinuxPaths;
-    default:
-      return []; // empty array, docker is still supported if available
+export default (platform = process.platform, ostype = process.env.OSTYPE) => {
+  var nixOnWindows = platform !== 'win32' && (ostype === 'cygwin' || ostype === 'msys');
+  var isWindows = platform === 'win32' || ostype === 'cygwin' || ostype === 'msys';
+
+  if (isWindows && nixOnWindows) {
+    return knownNixOnWindowsPaths;
   }
+  if (isWindows) {
+    return knownWindowsPaths;
+  }
+
+  if (platform === 'linux') {
+    return knownLinuxPaths;
+  }
+
+  return []; // empty array
 };
--- src/find-sqlcmd/get-known-paths.test.js
+++ src/find-sqlcmd/get-known-paths.test.js
@@ -1,16 +1,28 @@
-import getKnownPaths, { knownWindowsPaths, knownLinuxPaths } from './get-known-paths';
+import getKnownPaths, {
+  knownWindowsPaths,
+  knownNixOnWindowsPaths,
+  knownLinuxPaths,
+} from './get-known-paths';
 
 describe('find-sqlcmd/get-known-paths.js', () => {
   it('will return default from process.platform', () => {
-    expect(getKnownPaths()).toEqual(getKnownPaths(process.platform));
+    expect(getKnownPaths()).toEqual(getKnownPaths(process.platform, process.env.OSTYPE));
   });
 
   it('will return expected default for windows', () => {
-    expect(getKnownPaths('win32')).toEqual(knownWindowsPaths);
+    expect(getKnownPaths('win32', null)).toEqual(knownWindowsPaths);
+  });
+
+  it('will return expected results for nix on windows (cygwin)', () => {
+    expect(getKnownPaths('other', 'cygwin')).toEqual(knownNixOnWindowsPaths);
+  });
+
+  it('will return expected results for nix on windows (msys)', () => {
+    expect(getKnownPaths('other', 'msys')).toEqual(knownNixOnWindowsPaths);
   });
 
   it('will return expected default for linux', () => {
-    expect(getKnownPaths('linux')).toEqual(knownLinuxPaths);
+    expect(getKnownPaths('linux', null)).toEqual(knownLinuxPaths);
   });
 
   it('will return empty array for other platforms', () => {
