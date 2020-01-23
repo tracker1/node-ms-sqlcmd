d4f20ea83f5bcb9178959b621f6a0fd4afd02c14
Michael J. Ryan <mryan@runbeck.net>
2020-01-22 18:13:21 -0700

    simplify find sqlcmd method

--- src/find-sqlcmd/index.js
+++ src/find-sqlcmd/index.js
@@ -5,27 +5,12 @@ import { SQLCMD_NOT_FOUND_ERROR } from '../errors';
 export const getPathSeparator = platform => (platform === 'win32' ? ';' : ':');
 
 export default async (platform = process.platform) => {
-  const originalPaths = process.env.PATH;
-  let sqlcmd = null;
+  // add knownpaths to environment
+  const pathSeparator = getPathSeparator(platform);
+  const paths = process.env.PATH.split(pathSeparator)
+    .concat(getKnownPaths(platform))
+    .join(pathSeparator);
 
-  try {
-    // add knownpaths to environment
-    const pathSeparator = getPathSeparator(platform);
-    const paths = process.env.PATH.split(pathSeparator);
-    process.env.PATH = paths.concat(getKnownPaths(platform)).join(pathSeparator);
-
-    // find sqlcmd in paths
-    sqlcmd = await which('sqlcmd');
-  } catch (_) {
-    sqlcmd = null;
-  }
-
-  // restore paths
-  process.env.PATH = originalPaths;
-
-  if (sqlcmd) {
-    return sqlcmd;
-  } else {
-    throw SQLCMD_NOT_FOUND_ERROR;
-  }
+  // find sqlcmd in paths
+  return which('sqlcmd', { path: paths }).catch(() => Promise.reject(SQLCMD_NOT_FOUND_ERROR));
 };
